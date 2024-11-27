8318149044b707d7beb6f04c1a522aa4dc848bd3
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-11-27 13:10:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 //your JS code here. If required.
 
-// Initialize the angle
 let angle = 0;
-
-// Get the line element
 const line = document.getElementById('line');
 
-// Function to update the rotation
 function rotateLine() {
     angle += 2;
     line.style.transform = `rotate(${angle}deg)`;
